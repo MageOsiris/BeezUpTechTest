3533ffd415be846031c7de455c35d7ced1066242
MageOsiris <32306399+MageOsiris@users.noreply.github.com>
2019-09-18 11:12:10 +0200

    Initial commit: End of Part1 - Step 1 Creation of the console part

--- ConsoleAppPart/Program.cs
+++ ConsoleAppPart/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Tools;
 
 namespace ConsoleAppPart
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Start Reading File");
+			Console.WriteLine("Enter a specifique path of csv file. If empty it use /InFile/bigfile.csv in ConsoleAppPart");
+
+			string pathIn = Console.ReadLine();
+
+			if (string.IsNullOrEmpty(pathIn) || string.IsNullOrWhiteSpace(pathIn))
+				pathIn = "../../../InFile/bigfile.csv";
+
+			try
+			{
+				WorkFile(pathIn);
+				Console.WriteLine();
+				Console.WriteLine("Reading finish");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine();
+				Console.WriteLine(ex.InnerException);
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				Console.WriteLine();
+				Console.WriteLine("Please enter to quit");
+				Console.ReadLine();
+			}
+		}
+
+		/// <summary>
+		/// Method do the work for the csv file.
+		/// I use 1 000 000 for the maxline (i test it with 80 mo of csv and it's really performant)
+		/// </summary>
+		/// <param name="info"></param>
+		/// <param name="maxline"></param>
+		public static async void WorkFile(string info, int maxline = 1000000)
+		{
+			// Get the file
+			var lines = File.ReadLines(info);
+			string directoryPath = Path.GetDirectoryName(info);
+			string filenameok = Path.GetFileNameWithoutExtension(info);
+
+			// Get number of lines of the file
+			var linesNumber = lines.Count();
+			Console.WriteLine("Number of lines :" + linesNumber);
+
+			// Get number of occurence
+			int numberOfOccurence = (linesNumber / maxline);
+			// Console.WriteLine("Number of occurences :" + numberOfOccurence);
+
+			// Prepare loop
+			int cptOccurence = 0;
+			int lastLines = linesNumber - ((numberOfOccurence) * maxline);
+
+			// Loop and call specifique method by batch of data/lines
+			while (cptOccurence <= numberOfOccurence)
+			{
+				if (cptOccurence == numberOfOccurence)
+					await Reader.ReadAndConsole(lines.Skip(cptOccurence * maxline).Take(lastLines));
+				else
+					await Reader.ReadAndConsole(lines.Skip(cptOccurence * maxline).Take(maxline));
+				cptOccurence++;
+			}
 		}
 	}
 }
---
+++ Tools/Reader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tools
+{
+	public class Reader
+	{
+		/// <summary>
+		/// Read each line and do specifique work on value on column to render result
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public async static Task ReadAndConsole(IEnumerable<string> data)
+		{
+			// Value of column C
+			int valueC = 0;
+			// Value of column D
+			int valueD = 0;
+			// Bool of parsing succeded colums C and D
+			bool successParseValueC = false;
+			bool successParseValueD = false;
+
+			foreach (string line in data)
+			{
+				string[] l = line.Split(";");
+				if (l.Length >= 5)
+				{
+					successParseValueC = int.TryParse(l[3], out valueC);
+					successParseValueD = int.TryParse(l[4], out valueD);
+					if (successParseValueC && successParseValueD && (valueC + valueD) > 100)
+						Console.WriteLine(string.Concat(l[1], l[2]));
+				}
+			}
+			return;
+		}
+
+		// Just for information
+		// In the beggining i wanted split the file beacause ReadAllLines take too much memory.
+		// But i skip it, and i just use ReadLines who uses a lot less memory
+		//public async static Task ReadAndParse(IEnumerable<string> data, string path, string prefixeFileName, int idFile)
+		//{
+		//	StreamWriter ffs = new StreamWriter(path + "\\" + prefixeFileName + idFile + ".csv");
+		//	foreach (string line in data)
+		//	{
+		//		ffs.WriteLine(line);
+		//	}
+		//	ffs.Close();
+		//	ffs.Dispose();
+		//	return;
+		//}
+	}
+}
